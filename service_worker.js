45c9f063803bb857810ea2896b62a1e4cb104b3a
kashiwagi-wataru <kashiwagiwataru224@gmail.com>
2019-12-23 14:24:55 +0900

    PWA追加”

---
+++ service_worker.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    '/username.github.io/',
+];
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service_worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+    });
+    }
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// キャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
